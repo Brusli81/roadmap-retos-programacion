90fea149b0738eded7606a6f7bb2329e44401c22
Brusli81 <115102395+Brusli81@users.noreply.github.com>
2024-06-28 21:13:22 +0200

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Bruce1981.java
@@ -0,0 +1,37 @@
+
+// 00 Sintaxis, Variables, Tipos de Datosy Hola Mundo
+
+public class Bruce1981 {
+    public static void main(String[] args) {
+
+        // URL del sitio Oficial = https://www.java.com/es/
+
+        // Esto es un comentario en una línea
+
+        /*
+         * Esto es un comentario
+         * multilínea
+         */
+
+        // Variable y constante
+        String myVariable = "Mi variable";
+        final String CONSTANTE = "Mi Constante";
+
+        // Crea variables representando todos los tipos de datos primitivos
+        byte b = 0;
+        short s = 1;
+        String cadena = "Esto es una cadena de texto";
+        int numero = 24;
+        float flotante = 0.12f;
+        double doble = 12.34;
+        char caracter = 'a';
+        long numeroLargo = 1200000000;
+        boolean esCierto = true;
+
+        // Imprime por pantalla saludo con el nombre del lenguaje
+        System.out.println("¡Hola, JAVA!");
+        String lenguaje = "Java";
+        System.out.println("¡Hola, " + lenguaje + "!");
+
+    }
+}
